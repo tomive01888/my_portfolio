7eaaae98e284c0f1421268654cdbed5279b0e5ae
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-10-08 20:46:47 +0200

    add clone from local folder

---
+++ js/data.mjs
@@ -0,0 +1,42 @@
+export const data = {
+  page1: {
+    media: {
+      src: "/assets/oslosm.png",
+      alt: "Visit Oslo Science Museum",
+    },
+    title: "Oslo Science Museum - OSM",
+    description: "Visit OSM and explore a vast and massive display of earths history, ranging from fossils to the sky",
+    link: "https://oslosm.netlify.app",
+  },
+  page2: {
+    media: {
+      src: "/assets/technomaze.png",
+      alt: "Visit Technomaze a crazy blog website covering all bs stuff",
+    },
+    title: "Technomaze",
+    description: "Technomaze, covers a great deal of self-made GPT blogs with fun and crazy content.",
+    link: "https://technomaze.netlify.app",
+  },
+  page3: {
+    media: {
+      src: "/assets/squareeyes.png",
+      alt: "Image description for Page 3",
+    },
+    title: "Square Eyes",
+    description: "Buy and watch a great range of movies and series from old to new. Discounts and more!",
+    link: "https://tomive01888.github.io/JavaScript-SquareEyes/",
+  },
+
+  // Add more pages as needed
+  /*
+    pageX: {
+      media: {
+        src: "/assets/imgX.png", // Local asset
+        alt: "Image description for Page X"
+      },
+      title: "Page X Title",
+      description: "This is a description for Page X",
+      link: "https://example.netlify.app"
+    }
+    */
+};
